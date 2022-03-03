1d3ee256b894e91a6746dd74985f6e5dc1b548f7
diesdasnicolas <80157943+diesdasnicolas@users.noreply.github.com>
2022-03-03 20:12:15 +0100

    App.jsx: Initialize client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './favicon.svg'
+import './style/app.min.css'
+
+function App() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
