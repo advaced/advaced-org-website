aae86a1a5b2950ac05ab8da1b88c6ece3b55e467
diesdasnicolas <diesdasnicolas@gmail.com>
2022-03-20 00:07:35 +0100

    App.jsx: Add basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import logo from './favicon.svg'
-import './style/app.min.css'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import './style/app.min.css';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/:language/wallets' component={Wallet} />
+        <Route exact path='/:language/get-vac' component={GetVac} />
+        <Route exact path='/:language/run-a-node' component={RunANode} />
+        <Route exact path='/:language/staking' component={Staking} />
+
+        <Route exact path='/:language/advaced' component={Advaced} />
+        <Route exact path='/:language/vac' component={Vace} />
+        <Route exact path='/:language/whitepaper' component={Whitepaper} />
+        <Route exact path='/:language/upgrades' component={Upgrades} />
+
+        <Route exact path='/:language/development/' component={Development} />
+        <Route exact path='/:language/documentation' component={Documentation} />
+
+        <Route exact path='/:language/support/' component={Support} />
+      </Switch>
+    </Router>
   )
 }
 
